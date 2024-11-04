02d486728f2473c10df5af7c215fd04ca82c4cee
jungheeeun <jungheeeun272@gmail.com>
2024-11-04 13:39:36 +0900

    pass : "아무 작업도 수행하지 않음"을 의미하는 키워드

--- Prac/prac1104.py
+++ Prac/prac1104.py
@@ -148,15 +148,11 @@ class FlyableAttackUnit(AttackUnit, Flyable): #! 다중 상속 : AttackUnit, Fly
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
-# # 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
-# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-# valkyrie.fly(valkyrie.name, "3시")
+# 건물
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, loaction):
+        pass 
+    
 
-# 벌쳐 : 지상 유닛, 기동성이 좋음
-vulture = AttackUnit("벌쳐", 80, 10, 20) 
-
-# 배틀크루저 : 공중 유닛, 체력 좋음. 공격력 좋음.
-battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
-
-vulture.move("11시")
-battlecruiser.move("9시")
+# 서플라이 디폿 : 건물, 1개 건물 = 8 유닛. 
+supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
