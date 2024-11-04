0493c812e3e5990fc798c9196d4dc903f65b51e1
jungheeeun <jungheeeun272@gmail.com>
2024-11-04 12:55:25 +0900

    클래스

--- Prac/prac1104.py
+++ Prac/prac1104.py
@@ -11,13 +11,13 @@
 # # 3자리 콤마 + +-부호 같이 붙이기
 # print("{0:+,}".format(10000000000))
 # print("{0:+,}".format(-10000000000))
-# # 3자리 콤마 + 부호 + 자리수 
-# # 빈자리는 ^ 로 표시 
+# # 3자리 콤마 + 부호 + 자리수
+# # 빈자리는 ^ 로 표시
 # print("{0:^<+30,}".format(10000000000))
 # # 소수점 출력
 # print("{0:f}".format(5/3))
 # # 소수점 특정 자리수 까지만 표시 (3째 자리에서 반올림)
-# print("{0:.2f}".format(5/3)) 
+# print("{0:.2f}".format(5/3))
 
 #* 파일 입출력
 # score_file = open("score.txt", "w", encoding="utf8")
@@ -41,13 +41,13 @@
 # print(score_file.readline())
 # score_file.close()
 
-#! 활용 
+#! 활용
 # score_file = open("score.txt", "r", encoding="utf8")
 # while True:
 #   line = score_file.readline()
 #   if not line:
 #     break
-#   print(line, end="") #*줄바꿈 없애기 
+#   print(line, end="") #*줄바꿈 없애기
 # score_file.close()
 
 # score_file = open("score.txt", "r", encoding="utf8")
@@ -58,7 +58,7 @@
 
 # score_file.close()
 
-#* pickle 
+#* pickle
 # import pickle
 # profile_file = open("profile.pickle", "wb")
 # profile = {"이름": "박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
@@ -86,7 +86,7 @@
 # # 불러온 객체의 속성 확인
 # print(f"이름: {loaded_person.name}, 나이: {loaded_person.age}")
 
-#* with  
+#* with
 # import pickle
 # with open("profile.pickle", "rb") as profile_file:
 #   print(pickle.load(profile_file))
@@ -94,7 +94,24 @@
 # with open("study.txt", "w", encoding="utf8") as study_file:
     # study_file.write("파일 만들고 글 쓰기")
 
-# 파일 글 가져오기 
-# with open("study.txt", "r", encoding="utf8") as study_file: 
+# 파일 글 가져오기
+# with open("study.txt", "r", encoding="utf8") as study_file:
 #   print(study_file.read())
 
+#** 클래스
+class Unit:
+    """
+    C0115: missing-class-docstring" 경고로, 
+    클래스 정의에 대한 docstring이 누락됨을 나타냄.
+    Python에서 docstring은 클래스, 함수, 모듈 등의 설명을 제공하는 문자열로,
+    주로 클래스의 목적이나 사용법을 설명하는 데 사용됨
+    """
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력{0}, 공격력 {1}".format(self.hp, self.damage))
+marine1 = Unit("마린", 40, 5)
+marine2 = Unit("마린", 40, 5)
+tank = Unit("탱크", 150, 35)
