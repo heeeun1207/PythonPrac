3e2d98fad7f3070d83f9db959d8595cebcbae4ec
jungheeeun <jungheeeun272@gmail.com>
2024-11-04 13:28:51 +0900

    다중 상속 (AttackUnit, Flyable)

--- Prac/prac1104.py
+++ Prac/prac1104.py
@@ -106,7 +106,7 @@ class Unit:
         self.hp = hp
 
 # 공격 유닛
-class AttackUnit(Unit):
+class AttackUnit(Unit): # Unit 을 상속 받음 
     def __init__(self, name, hp, damage):
         Unit.__init__(self, name, hp)
         self.damage = damage
@@ -122,10 +122,28 @@ class AttackUnit(Unit):
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
 
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
 
-firebat1.damaged(25)
-firebat1.damaged(25)
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
-#* 상속 
+#* 다중 상속
+# 날 수 있는 기능을 가진 클래스 
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+        
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+        
+# 공중 공격 유닛 클래스 
+class FlyableAttackUnit(AttackUnit, Flyable): #! 다중 상속 : AttackUnit, Flyable 
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
