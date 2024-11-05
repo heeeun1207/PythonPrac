03346e875f6a1cd134d0d7a0ae47546bd1bbd92d
jungheeeun <jungheeeun272@gmail.com>
2024-11-05 20:36:58 +0900

    모듈

--- Prac/prac1105.py
+++ Prac/prac1105.py
@@ -26,25 +26,42 @@
 #     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
 
 #* 사용자 정의 예외 처리 
-class BigNumberError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
 
-    def __str__(self):
-        return self.msg
+#     def __str__(self):
+#         return self.msg
 
-try:
-    print("한 자리 숫자 나누기 전용 계산기")
-    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
-    num2 = int(input("두 번째 숫자를 입력하세요 : "))
-    if num1 >= 10 or num2 >= 10:
-        raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
-    print ("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-except ValueError:
-    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
-except BigNumberError as err:
-    print("에러가 발생했습니다. 한 자리 숫자만 입력하시오.")
-    print(err)
-finally:
-    print("계산기를 이용해 주셔서 감사합니다.")
-    
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print ("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생했습니다. 한 자리 숫자만 입력하시오.")
+#     print(err)
+# finally:
+#     print("계산기를 이용해 주셔서 감사합니다.")
+
+#*모듈
+import theater_module
+theater_module.price(3)
+theater_module.price_morning(4)
+theater_module.price_soldier(5)
+
+import theater_module as mv
+mv.price(3)
+mv.price_morning(3)
+mv.price_soldier(3)
+
+from theater_module import *
+price(3)
+price_morning(3)
+price_soldier(3)
+
+
