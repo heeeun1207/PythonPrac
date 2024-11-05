d731bd528d59a57f8584178cd89747e7dc93e394
jungheeeun <jungheeeun272@gmail.com>
2024-11-05 16:41:29 +0900

    클래스 생성자, 인스턴스 메서드 기능 학습

--- Prac/Quiz.py
+++ Prac/Quiz.py
@@ -125,10 +125,44 @@ print("커피 당첨자 : {0}".format(winners[1:]))
 
 # 1주차 부터 50주차 까지 보고서 파일을 만드는 프로그램을 작성하시오.
 # 조건: 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만든다. 
-for i in range(1, 4):
-    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
-        report_file.write(" - {0} 주차 주간보고 -".format(i))
-        report_file.write("\n부서 : ")
-        report_file.write("\n업무 요약 : ")
+# for i in range(1, 4):
+#     with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+#         report_file.write(" - {0} 주차 주간보고 -".format(i))
+#         report_file.write("\n부서 : ")
+#         report_file.write("\n업무 요약 : ")
 
+#!08
+# 주어진 코드를 활용해 부동산 프로그램을 작성하시오.
 
+# (출력 예제)
+# 총 3대의 매물이 있습니다.
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 전세 5억 2007년
+# 송파 빌라 월세 500/50 2000년
+
+# [코드]
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location =location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year =completion_year
+    # 매물 정보 표시
+    
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type\
+            , self.price, self.completion_year)
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세" ,"5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+for house in houses:
+    house.show_detail()
