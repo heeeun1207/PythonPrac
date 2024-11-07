2a6c80241f0caba4ca341b545a7c70509cc6d417
jungheeeun <jungheeeun272@gmail.com>
2024-11-07 12:36:35 +0900

    개인 모듈 만들기

--- Prac/Quiz.py
+++ Prac/Quiz.py
@@ -178,30 +178,46 @@ print("{0}의 비밀번호는 {1} 입니다." .format(url, password))
 #         출력 메시지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
 
 #[코드]
-class SoldOutError(Exception):
-    pass
+# class SoldOutError(Exception):
+#     pass
 
-chicken = 10
-waiting = 1 # 홀 안에는 현재 만석. 대기번호 1부터 시작
-while(True):
-    try:
-        print("[남은 치킨] : {0}".format(chicken))
-        order = int(input("치킨 몇 마리를 주문하시겠습니까?"))
-        if order > chicken : # 남은 치킨보다 주문량이 많을 때
-            print("재료가 부족합니다.")
-        elif order <= 0:
-            raise ValueError
-        else:
-            print("[대기번호 {0}] {1} 마리 주문이 완료 되었습니다."\
-                .format(waiting, order))
-            waiting += 1
-            chicken -= order
+# chicken = 10
+# waiting = 1 # 홀 안에는 현재 만석. 대기번호 1부터 시작
+# while(True):
+#     try:
+#         print("[남은 치킨] : {0}".format(chicken))
+#         order = int(input("치킨 몇 마리를 주문하시겠습니까?"))
+#         if order > chicken : # 남은 치킨보다 주문량이 많을 때
+#             print("재료가 부족합니다.")
+#         elif order <= 0:
+#             raise ValueError
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료 되었습니다."\
+#                 .format(waiting, order))
+#             waiting += 1
+#             chicken -= order
         
-        if chicken == 0:
-            raise SoldOutError
+#         if chicken == 0:
+#             raise SoldOutError
         
-    except ValueError:
-        print("잘못된 값을 입력하였습니다.")
-    except SoldOutError:
-        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
-        break
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except SoldOutError:
+#         print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         break
+
+#!10
+# 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오.
+
+# 조건 : 모듈 파일명은 byme.py 로 작성
+
+# (모듈 사용 예제)
+import byme
+byme.sign()
+
+# (출력 예제)
+# 이 프로그램은 ""의해 만들어졌습니다.
+# 유튜브 :
+# 이메일 : 
+
+# 1. 모듈이 들어갈 파일 생성해주고, 가져오면 되는 간단한 문제. 
