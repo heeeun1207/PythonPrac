cdb5d24a912fbab32fe37ea47ea043c31107958a
jungheeeun <jungheeeun272@gmail.com>
2024-11-07 13:25:39 +0900

    오타 수정

--- arcadeGame/1_create_frame.py
+++ arcadeGame/1_create_frame.py
@@ -6,7 +6,7 @@ pygame.init() # 초기화 (반드시 필요)
 # 화면 크기 설정
 screen_width = 480 # 가로 크기
 sereen_height = 640 # 세로 크기
-screem = pygame.display.set_mode((screen_width, sereen_height))
+screen = pygame.display.set_mode((screen_width, sereen_height))
 
 # 화면 타이틀 설정
 pygame.display.set_caption("retro Game") # 게임 이름
