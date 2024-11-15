aeb73a160c7fcf252fbf5ad38796a50da8a6ef74
jungheeeun <jungheeeun272@gmail.com>
2024-11-15 13:26:17 +0900

    하단 저장경로 프레임 생성

--- gui_project/1_create_layout.py
+++ gui_project/1_create_layout.py
@@ -24,6 +24,17 @@ list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand
 list_file.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=list_file.yview)
 
+# 저장 경로 프레임
+path_frame = LabelFrame(root, text="저장경로")
+path_frame.pack()
+
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # 높이 변경
+
+btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path.pack(side="right")
+
+
 root.resizable(False, False)
 root.mainloop()
 
