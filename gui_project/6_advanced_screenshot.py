e04ab3c03207c95632a36a571b66751801693d01
heeeun <jungheeeun272@gmail.com>
2024-11-20 16:23:56 +0900

    윈도우환경 캡쳐 실행 확인※

--- gui_project/6_advanced_screenshot.py
+++ gui_project/6_advanced_screenshot.py
@@ -3,6 +3,7 @@ import time
 from PIL import ImageGrab
 from pynput import keyboard
 
+#! 윈도우 확인 > Screenshot saved as ~ Desktop/image_20241120_162237.png
 def screenshot():
     curr_time = time.strftime("_%Y%m%d_%H%M%S")
     save_path = os.path.expanduser(f"~/Desktop/image{curr_time}.png")  # 바탕화면에 저장
