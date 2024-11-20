ae9f673c231a9ca4027801ba84a436fd0c014e35
jungheeeun <jungheeeun272@gmail.com>
2024-11-20 12:39:45 +0900

    윈도우 확인해보기☑️

---
+++ gui_project/6_advanced_screenshot.py
@@ -0,0 +1,21 @@
+import os
+import time
+from PIL import ImageGrab
+from pynput import keyboard
+
+def screenshot():
+    curr_time = time.strftime("_%Y%m%d_%H%M%S")
+    save_path = os.path.expanduser(f"~/Desktop/image{curr_time}.png")  # 바탕화면에 저장
+    img = ImageGrab.grab()
+    img.save(save_path)
+    print(f"Screenshot saved as {save_path}")
+
+def on_press(key):
+    try:
+        if key.char == 'f':  # 'f' 키를 눌렀을 때
+            screenshot()
+    except AttributeError:
+        pass
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
